79800dfda1df6372b87bc53d88f1736595160e90
Darshit Limbad <121852711+LimbadDarshit@users.noreply.github.com>
2024-03-21 01:05:03 +0530

    Update CSUM.C

--- C_lanprog/CSUM.C
+++ C_lanprog/CSUM.C
@@ -3,7 +3,10 @@
 
 void main()
 {
-	int arr[3][3],i,j,sum1=0,sum2=0,sum3=0;
+	int arr[3][3],i,j,sum[3];
+
+sum[0]=0;sum[1]=0;sum[2]=0;
+
 	clrscr();
 	for(i=0 ; i<3 ; i++)
 	{
@@ -11,25 +14,11 @@ void main()
 		{
 		printf("arr[%d][%d]:",i,j);
 		scanf("%d",&arr[i][j]);
-		}
-	}
-	for(j=0 ; j<3 ; j++)
-	{
-		for(i=0 ; i<3 ; i++)
-		{
-			if(j==0)
-			sum1+=arr[i][j];
-
-			else if(j==1)
-			sum2+=arr[i][j];
-
-			else
-			sum3+=arr[i][j];
-
+sum[i]+=arr[i][j];
 		}
 	}
 
-	printf("column sum is sum1:%d\t sum2:%d \tsum3:%d",sum1,sum2,sum3);
+	printf("column sum is sum1:%d\t sum2:%d \tsum3:%d",sum[0],sum[1],sum[2]);
 
 	getch();
 }
